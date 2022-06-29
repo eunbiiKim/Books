fc208c26b17e83e6b80bad6c70ab89a2858d61d7
eunbiiKim <eunbikim0807@gmail.com>
2022-06-29 13:14:26 +0900

    ShowNewBooksViewController - navigation bar 와 table view 생성

--- Books/SearchBooksViewController.swift
+++ Books/SearchBooksViewController.swift
@@ -5,12 +5,7 @@ class SearchBooksViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .gray
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print(#function)
+        self.view.backgroundColor = .white
     }
 }
 
--- Books/ShowNewBooksViewController.swift
+++ Books/ShowNewBooksViewController.swift
@@ -2,15 +2,40 @@ import UIKit
 
 class ShowNewBooksViewController: UIViewController {
     
+    let tableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+        
+        self.navigationItem.title = "New Books"
+        
+        self.navigationController?.navigationBar.backgroundColor = .white
+        
+        self.navigationController?.navigationBar.barTintColor = .white
+        
+        self.tableView.delegate = self
+        
+        self.tableView.dataSource = self
+        
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.tableView.frame = self.view.bounds
+        
+        self.view.addSubview(self.tableView)
+    }
+}
+
+extension ShowNewBooksViewController: UITableViewDelegate {}
+
+extension ShowNewBooksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print(#function)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
 
